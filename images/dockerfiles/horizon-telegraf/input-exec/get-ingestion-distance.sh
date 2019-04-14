6529a291c0927f4888aadab1670570681f643330
Ory Band <oryband@users.noreply.github.com>
2019-04-14 15:47:11 +0300

    cleanup horizon telegraf input.exec scripts
    docs and use printf instead of echo

--- images/dockerfiles/horizon-telegraf/input-exec/get-ingestion-distance.sh
+++ images/dockerfiles/horizon-telegraf/input-exec/get-ingestion-distance.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
-result=$(curl localhost)
-core=$(echo $result|jq '.core_latest_ledger')
-horizon=$(echo $result|jq '.history_latest_ledger')
-echo 'ingestion_distance distance='$(($core-$horizon))
+#!/bin/bash
+# this script calculates and prints the difference between the latest ledger in core
+# and the latest ingested ledger in horizon (history latest ledger)
+
+set -e
+
+result=$(curl -sS ${1:-localhost})
+
+core=$(echo $result | jq -r '.core_latest_ledger')
+horizon=$(echo $result | jq -r '.history_latest_ledger')
+
+printf 'ingestion_distance distance=%d\n' $(( $core - $horizon ))
