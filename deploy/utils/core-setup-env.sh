665082ceca5358a7bcc091394a7e1a18b37aa573
Yogev Heskia <yogev911@gmail.com>
2019-07-23 12:46:07 +0300

    clean node DB and setup core application for first use

--- deploy/utils/core-setup-env.sh
+++ deploy/utils/core-setup-env.sh
@@ -2,7 +2,11 @@
 sudo rm -rf /data/postgresql
 sudo rm -rf /data/stellar-core/buckets
 sudo docker-compose -f /data/docker-compose.yml up -d stellar-core-db
+sleep 14
 sudo docker-compose -f /data/docker-compose.yml run --rm stellar-core --newdb
+sleep 2
 sudo docker-compose -f /data/docker-compose.yml run --rm stellar-core --forcescp
+sleep 2
 sudo docker-compose -f /data/docker-compose.yml run --rm stellar-core --newhist local
+sleep 2
 sudo docker-compose -f /data/docker-compose.yml up -d
---
+++ deploy/utils/horizon-setup-env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+sudo rm -rf /data/postgresql
+sudo rm -rf /data/horizon-volumes
+sudo docker-compose -f /data/docker-compose.yml up -d horizon-db
+sleep 14
+sudo docker-compose -f /data/docker-compose.yml run --rm horizon db init
+sleep 2
+sudo docker-compose -f /data/docker-compose.yml up -d
